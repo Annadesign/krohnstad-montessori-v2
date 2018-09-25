101ba2202d0fa83e8a1d4c264909ed15d454be16
annadesign <konto@annadesign.no>
2018-09-25 16:01:43 +0200

    test

--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -1,12 +1,9 @@
 class Moderator < ApplicationRecord
 	has_secure_password
 	has_many :articles
-	before_save { self.email = email.downcase }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  	validates :username, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+
 	validates :fullname, presence: true
+	validates :username, presence: true
 	validates :password, presence: true, length: { minimum: 6 }
 
 end

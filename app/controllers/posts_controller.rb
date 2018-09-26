379f303c5576efff493501edbcd3c52aad6a99b3
annadesign <konto@annadesign.no>
2018-09-26 13:22:54 +0200

    added galleries

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,12 @@ class PostsController < ApplicationController
 
 	def index
 		@parents = Parent.all
+
+		@article1 = Article.find(1)
+		@article2 = Article.find(2)
+		#localhost		
+		#@article1 = Article.find(11)
+		#@article2 = Article.find(6)
 	end
 
 end

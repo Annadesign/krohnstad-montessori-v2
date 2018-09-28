ff56d17e7758ac488f2f870437b2d01bdf2b1498
annadesign <konto@annadesign.no>
2018-09-28 13:12:11 +0200

    barnehage

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,7 @@ class Article < ApplicationRecord
   scope :categorized, -> {order(:category_id, :id)}
 
   scope :barnehagen, -> { where(publish: true, :category_id => '1') }
-  scope :ommontessori, -> { where(publish: true, :category_id => '3') }
+  scope :ommontessori, -> { where(publish: true, :category_id => '3').order(:id) }
   scope :satsingsomrade, -> { where(publish: true, :category_id => '4').order(:id) }
   scope :skole, -> { where(publish: true, :category_id => '2') }
   scope :informasjon, -> { where(publish: true, :category_id => '5') }

be1c33b8c239f1a515456948dff2b52e8fbb4e23
annadesign <konto@annadesign.no>
2018-10-02 12:39:26 +0200

    css templates

--- app/controllers/barnehagen_controller.rb
+++ app/controllers/barnehagen_controller.rb
@@ -4,10 +4,6 @@ class BarnehagenController < ApplicationController
   def index
     @barnehagen = Article.barnehagen
 
-    @barnehage1 = Article.find(1)
-    @barnehage12 = Article.find(12)
-    @barnehage13 = Article.find(13)
-
    
   end
 

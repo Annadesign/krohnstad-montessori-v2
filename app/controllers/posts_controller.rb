9f1ca8e7e1a25b33901c5d9b454c7a098380eece
annadesign <konto@annadesign.no>
2018-10-15 09:54:09 +0200

    colors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,8 @@ class PostsController < ApplicationController
 	def index
 		@parents = Parent.all
 
-		@article1 = Article.find(1)
-		@article2 = Article.find(3)
+		@article1 = Article.find(28)
+		@article2 = Article.find(29)
 		#localhost		
 		#@article1 = Article.find(11)
 		#@article2 = Article.find(6)

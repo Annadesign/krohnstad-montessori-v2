ba129f9a035710fd151d734f0276e3f041554f97
annadesign <konto@annadesign.no>
2018-10-15 09:58:59 +0200

    fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,8 @@ class PostsController < ApplicationController
 	def index
 		@parents = Parent.all
 
-		@article1 = Article.find(28)
-		@article2 = Article.find(29)
+		@article1 = Article.find(12)
+		@article2 = Article.find(13)
 		#localhost		
 		#@article1 = Article.find(11)
 		#@article2 = Article.find(6)

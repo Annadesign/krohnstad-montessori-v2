8b354fcfe98d037dd8936b8a9093cfefedfa50a3
annadesign <konto@annadesign.no>
2018-10-29 10:00:39 +0100

    endre innstillinger

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ class Admin::DashboardController < Admin::ApplicationController
   def index
   	@schools = School.all
   	@departments = Department.all
-  	
+  	#@children = Child.all
+  	#@parents = Parent.all
   end
+
 end

dc037cd4927003ab52bfb74ed4b37c8ba0ff29ab
annadesign <konto@annadesign.no>
2018-11-16 13:24:12 +0100

    badge

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,7 +3,7 @@ class Admin::DashboardController < Admin::ApplicationController
   def index
   	@schools = School.all
   	@departments = Department.all
-  	#@children = Child.all
+  	@info = Info.where("length(content) > 20")
   	#@parents = Parent.all
   end
 

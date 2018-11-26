342b3dba1d9d9b5a90cd5d276f27fcad6fb8f83a
annadesign <konto@annadesign.no>
2018-11-26 13:52:55 +0100

    docs

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < ApplicationRecord
   belongs_to :department
 
   validates :title, presence: true
-  validates :description, presence: true
+  #validates :description, presence: true
   validates :document, presence: true
 
   mount_uploader :document, DocumentUploader
